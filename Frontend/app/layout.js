b0b93fd42758022ab1535c4db87355566e0ad221
schBenedikt <benedikt.schaechner@web.de>
2026-03-29 22:39:01 +0200

    Add custom contrast color picker to design settings

--- Frontend/app/layout.js
+++ Frontend/app/layout.js
@@ -19,13 +19,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="de" data-theme="classic">
+    <html lang="de" data-theme="gold" data-mode="light">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
-      <body className={`${spaceGrotesk.variable} ${ibmPlexMono.variable}`} data-theme="classic">
+      <body className={`${spaceGrotesk.variable} ${ibmPlexMono.variable}`} data-theme="gold" data-mode="light">
         {children}
       </body>
     </html>
